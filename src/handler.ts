1f7f34a9cd7d7fd956b0c1930f1fd93c34023a71
Raju Sarvasiddi <rajusvsp@gmail.com>
2024-10-12 16:29:25 -0400

    change

--- src/handler.ts
+++ src/handler.ts
@@ -3,7 +3,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from TypeScript Lambda!' }),
+    body: JSON.stringify({ message: 'Hello from TypeScript Lambdas!' }),
   };
   return response;
 };
